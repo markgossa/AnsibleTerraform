959cf168f0f32db42fb2e486362fe71740f24eea
Mark Gossa <mark@gossa.co.uk>
2018-03-30 23:11:40 +0100

    Changes custom VM script extension fileUri to a variable

--- roles/common/files/vmDeployment.tf
+++ roles/common/files/vmDeployment.tf
@@ -203,7 +203,7 @@ resource "azurerm_virtual_machine_extension" "vm1" {
   auto_upgrade_minor_version =  true
   settings = <<SETTINGS
     {
-        "fileUris": "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
+        "fileUris": "${var.configureRemotingForAnsibleScript}"
     }
   SETTINGS
   protected_settings = <<PROTECTED_SETTINGS
