8fe6dbbf4d15e7b37970f86c118e60cd35942ee8
Mark Gossa <mark@gossa.co.uk>
2018-04-01 01:03:01 +0100

    Simplify azurerm_network_interface.dc1-update

--- roles/firstDomainControllerInfrastructure/files/dc1Infrastructure.tf
+++ roles/firstDomainControllerInfrastructure/files/dc1Infrastructure.tf
@@ -263,9 +263,6 @@ resource "azurerm_network_interface" "dc1-update" {
 
   ip_configuration {
     name                          = "ipconfig1"
-    subnet_id                     = "${azurerm_subnet.subnet1.id}"
-    private_ip_address_allocation = "static"
-    private_ip_address            = "${var.dc1IPAddress}"
     public_ip_address_id          = "${azurerm_public_ip.dc1.id}"
   }
 
