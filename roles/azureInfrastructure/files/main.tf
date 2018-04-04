3211cedcafc58c8b1d3be1cb33f7bc60d4094ef9
Mark Gossa <mark.gossa@wanstor.com>
2018-04-04 13:47:02 +0100

    Add variables into main.tf

--- roles/azureInfrastructure/files/main.tf
+++ roles/azureInfrastructure/files/main.tf
@@ -23,6 +23,82 @@ variable "resourceGroupLocation" {
     type = "string"
 }
 
+variable "virtualNetworkName" {
+    type = "string"
+}
+
+variable "virtualNetworkDnsServer1" {
+    type = "string"
+}
+
+variable "virtualNetworkDnsServer2" {
+    type = "string"
+}
+
+variable "virtualNetworkAddressSpace" {
+    type = "string"
+}
+
+variable "subnetName" {
+    type = "string"
+}
+
+variable "subnetNetworkID" {
+    type = "string"
+}
+
+variable "managementIP" {
+    type = "string"
+}
+
+variable "storageAccountTier" {
+    type = "string"
+}
+
+variable "storageAccountReplicationType" {
+    type = "string"
+}
+
+variable "storageAccountName" {
+    type = "string"
+}
+
+variable "dc1Name" {
+    type = "string"
+}
+
+variable "dc1Size" {
+    type = "string"
+}
+
+variable "dc1DiskCaching" {
+    type = "string"
+}
+
+variable "dc1ManagedDiskType" {
+    type = "string"
+}
+
+variable "vmUserName" {
+    type = "string"
+}
+
+variable "vmPassword" {
+    type = "string"
+}
+
+variable "vmSku" {
+    type = "string"
+}
+
+variable "dc1IPAddress" {
+    type = "string"
+}
+
+variable "vmOffer" {
+    type = "string"
+}
+
 # Configure the Azure Provider
 provider "azurerm" { 
   subscription_id   = "${var.subscription_id}"
